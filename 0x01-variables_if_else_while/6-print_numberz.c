9059c99c6f582924e1d7a9bd468e48fb0208b152
divine016 <koutidivine@gmail.com>
2022-09-09 17:36:55 -0700

    this is for task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:  A lways 0 (Success)
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+
+	return (0);
+}
