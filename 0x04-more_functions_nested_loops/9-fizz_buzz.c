ea7143c09e232114469df08921ded55ff807c81e
divine016 <koutidivine@gmail.com>
2022-09-17 08:13:40 -0700

    this si for task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
