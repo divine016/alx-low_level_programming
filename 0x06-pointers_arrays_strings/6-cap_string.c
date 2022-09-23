0a54f540cdd0db1d223aef48959e621c2cee050f
divine016 <koutidivine@gmail.com>
2022-09-22 19:03:58 -0700

    This is for task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,35 @@
 #include "main.h"
-int check_seperators(char c);
+/**
+ * cap_string - Capitalizes all words of a string.
+ * Return: A pointer to the changed string.
+*/
+char *cap_string(char *)
+{
+	int i = 0;
 
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+		i++;
+	
+	}
+
+	return (str);
+}
