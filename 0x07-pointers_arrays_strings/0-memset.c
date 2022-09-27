d7c3b23e97553a19045e12dc6947b8ef76c3768f
divine016 <koutidivine@gmail.com>
2022-09-27 09:50:18 -0700

    this is for task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,17 +2,17 @@
 /**
  * _memset - a function that fills memory with constant bytes
  * @s: location to fill
- * @c: char to fill location with
- * @numb: number of bytes to fill
+ * @b: char to fill location with
+ * @n: number of bytes to fill
  * Return: returns pointer to location filled
 */
-char *_memset(char *s, char c, unsigned int numb)
+char *_memset(char *s, char b, unsigned int n)
 {
 	char *start = s;
 
-	while (numb--)
+	while (n--)
 	{
-		*s = c;
+		*s = b;
 		s++;
 	}
 	return (start);
