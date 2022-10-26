8ce2804bfcac91592bb2efedbaca781c871be6c2
divine016 <koutidivine@gmail.com>
2022-10-25 20:27:46 -0700

    this is for task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,20 +1,31 @@
 #include "main.h"
 /**
- * wildcmp - A function that compares two strings
- * @s1: First input string
- * @s2: Second input string
- * @s2: Second input string
- * Return: returns 1 if identical and 0 if ortherwise
-*/
+ * wildcmp - compare two strings with possible wildcards
+ * @s1: string 
+ * @s2: string
+ * Return:int 0 or 1
+ */
+
 int wildcmp(char *s1, char *s2)
 {
-	if (!*s1 && !*s2)
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
-		return (1);
-	if (*s2 == '*' && *(s1 + 1) && *s2)
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
 		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
 	return (0);
 }
