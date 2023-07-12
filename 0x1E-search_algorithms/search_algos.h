9e902e40833a036dd33741bba00baea77f27439c
divine016 <koutidivine@gmail.com>
2023-07-12 08:23:48 -0700

    contains the functions def

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,43 @@
+#ifndef _SORTING_ALGOS_H_
+#define _SORTING_ALGOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * Description: singly linked list for node
+ * structure for low level
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct skiplist_s - Singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pionter to next node
+ * @express: Pionter to the next node in express
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
